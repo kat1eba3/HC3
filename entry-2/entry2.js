60c74453d43cbf199c9d4b9105d6ce5c97c259ce
kat1eba3 <baek683@newschool.edu>
2023-12-06 15:13:32 -0500

    entry2

---
+++ entry-2/entry2.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const backgroundContainer = document.getElementById('background-container');
+    const cutout = document.getElementById('cutout');
+    let currentBackgroundIndex = 0;
+  
+    const backgrounds = [
+        'background1',
+        'background2',
+        'background3',
+        'background4',
+        'background5',
+        'background6',
+        // add more backgrounds as needed
+    ];
+  
+    cutout.addEventListener('click', function () {
+        currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
+  
+        // Remove all existing background classes
+        backgroundContainer.classList.remove('background1', 'background2', 'background3', 'background4', 'background5', 'background6');
+  
+        // Add the current background class
+        backgroundContainer.classList.add(backgrounds[currentBackgroundIndex]);
+    });
+  });
+  
